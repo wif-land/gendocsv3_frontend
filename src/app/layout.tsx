2021e8f31a1421ccf857804d64380d9abb80fa19
Emi1213 <emi1213galarza@gmail.com>
2023-12-27 15:18:15 -0500

    Install next ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,9 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang="en">
-    <body className={inter.className}>{children}</body>
+    <body className={inter.className}><Providers>
+      <main>{children}</main>
+      </Providers></body>
   </html>
 )
 
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import {Button} from '@nextui-org/react'
+
+export default function Page() {
+  return (
+    <div>
+      <Button color="danger">Click me</Button>
+    </div>
+  )
+}
