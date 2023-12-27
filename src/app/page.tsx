ef50b8e21a419f0829a6150784cf748b6fedf5aa
Emi1213 <emi1213galarza@gmail.com>
2023-12-27 18:19:22 -0500

    Add buttons and inputs components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import Button  from '../components/Button'
+import Inputs from '@/components/Inputs'
 
 const Home = () => (
   <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <Button/>
+    <Button size='lg'/>
+    <Inputs type='email'label= 'Email'variant='underlined' placeholder='Ingrese un correo' regex='/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/' />
   </main>
 )
 
--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import {Button} from '@nextui-org/react'
-
-export default function Page() {
-  return (
-    <div>
-      <Button color="danger">Click me</Button>
-    </div>
-  )
-}
