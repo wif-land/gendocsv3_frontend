22f4e660747506604ec3288eb3d2cb663f6ca880
Emi1213 <emi1213galarza@gmail.com>
2023-12-28 18:20:03 -0500

    Login styles done, User global state to be done

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -9,37 +9,41 @@ export const LoginForm = () => {
   const submitting = formik.isSubmitting
 
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen ">
       <div
-        className="flex-1 bg-cover bg-center bg-no-repeat relative"
+        className="flex-1 bg-contain bg-center bg-repeat  "
         style={{
           backgroundImage: `url('${FondoFISEI.src}')`,
         }}
       />
-      <div className="flex-1 flex flex-col justify-center items-center gap-4">
+      <div className="flex-1 flex flex-col justify-center items-center">
         <img
           src="https://fisei.uta.edu.ec/v4.0/images/logo-sitio-fisei-2020.png"
           alt="logo-fisei"
           loading="lazy"
-          className="w-64 h-64"
+          className="w-4/6 h-auto pb-12 "
         />
-        <form onSubmit={formik.handleSubmit}>
-          <Input
-            id="email"
-            name="email"
-            type="email"
-            label="Email"
-            variant="underlined"
-            placeholder="Ingrese un correo"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            errorMessage={
-              formik.touched.email && formik.errors.email
-                ? formik.errors.email
-                : ''
-            }
-          />
+
+        <form onSubmit={formik.handleSubmit} className='w-1/2 justify-items-center pl-16 pb-32'>
+          <div className='grid w-6/6 justify-items-center '>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              variant="underlined"
+              placeholder="Ingrese un correo"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              size='lg'
+              errorMessage={
+                formik.touched.email && formik.errors.email
+                  ? formik.errors.email
+                  : ''
+              }
+              className='w-full'
+            />
           <Input
             id="password"
             name="password"
@@ -50,15 +54,19 @@ export const LoginForm = () => {
             value={formik.values.password}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
+            size='lg'
             errorMessage={
               formik.touched.password && formik.errors.password
                 ? formik.errors.password
                 : ''
             }
           />
-          <Button type="submit" size="lg" disabled={submitting}>
+          
+          
+          <Button type="submit" size="lg" disabled={submitting} radius='none' className='w-3/4 m-7 bg-red-800 text-white font-semibold justify-center' >
             Ingresar
           </Button>
+          </div>
         </form>
       </div>
     </div>
