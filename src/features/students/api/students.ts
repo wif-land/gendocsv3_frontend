bd6007cef39e142de0ea121ab2565411e2251625
Emi1213 <emi1213galarza@gmail.com>
2024-01-03 14:40:33 -0500

    Students view to be done

---
+++ src/features/students/api/students.ts
@@ -0,0 +1,14 @@
+// import { API_ROUTES } from "@/shared/constants/appApiRoutes";
+// import { AxiosClient } from "@/shared/utils/AxiosClient";
+// import { HTTP_STATUS_CODES } from "@/shared/utils/app-enums";
+
+// export class StudentsApi {
+//     static fetchStudents = async (): Promise<  {
+//         // const result = await AxiosClient.get(API_ROUTES.STUDENTS.GET_ALL);
+//         // const { status, data } = result;
+//         // if (status === HTTP_STATUS_CODES.UNAUTHORIZED)
+//         //     return { status };
+//         // return { status, students: data.content };
+//     }> =>{};
+
+// }
