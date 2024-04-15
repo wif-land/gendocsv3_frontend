1ec8ca2314864dcaef398fad17ac6d22e7a6a976
SrPabvliss <pablomartinvillacres@gmail.com>
2024-04-15 11:40:56 -0500

    Data and domain files refactored

---
+++ src/features/default-members/data/models/DefaultMembersModel.ts
@@ -0,0 +1,30 @@
+import { IDefaultMembers, IMember } from '../../domain/entities/DefaultMembers'
+
+export class DefaultMemberModel implements IDefaultMembers {
+  order: number
+  positionName: string
+  member: IMember
+
+  constructor(props: IDefaultMembers) {
+    this.order = props.order
+    this.positionName = props.positionName
+    this.member = props.member
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static fromJson(json: Record<string, any>): DefaultMemberModel {
+    return new DefaultMemberModel({
+      order: json.order,
+      positionName: json.positionName,
+      member: json.member,
+    })
+  }
+
+  toJson(): Record<string, unknown> {
+    return {
+      order: this.order,
+      positionName: this.positionName,
+      member: this.member,
+    }
+  }
+}
