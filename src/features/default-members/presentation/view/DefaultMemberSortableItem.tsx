d351096b6f629a9f54b185d2ffcf15b4c1e9628f
SrPabvliss <pablomartinvillacres@gmail.com>
2024-04-15 23:45:53 -0500

    Default member view started, hook methods new to be checked and inputs must be changed

--- src/features/default-members/presentation/view/DefaultMemberSortableItem.tsx
+++ src/features/default-members/presentation/view/DefaultMemberSortableItem.tsx
@@ -1,61 +1,85 @@
 import { useSortable } from '@dnd-kit/sortable'
-import { IMember } from '../../domain/entities/DefaultMembers'
 import { CSS } from '@dnd-kit/utilities'
 
-import { TextField, Button, Input } from '@mui/material'
-import { useState } from 'react'
+import { Button, Grid, Card, Stack, Box } from '@mui/material'
+import { RHFTextField } from '../../../../shared/sdk/hook-form'
+import { Controller, FieldArrayWithId } from 'react-hook-form'
 
 export const DefaultMemberSortableItem = ({
-  user,
-  onUpdate,
+  defaultMember,
   onDelete,
+  control,
+  index,
 }: {
-  user: IMember
-  onUpdate: (id: string, field: keyof IMember, value: string) => void
+  defaultMember: FieldArrayWithId
   onDelete: (id: string) => void
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  control: any
+  index: number
 }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: user.dni })
+    useSortable({ id: defaultMember.id })
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
   }
-
-  // Manejar estado local para los inputs
-  const [firstName, setFirstName] = useState(user.firstName)
-  const [secondLastName, setSecondLastName] = useState(user.secondLastName)
-
-  const handleFirstNameChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setFirstName(event.target.value)
-    onUpdate(user.dni, 'firstName', event.target.value)
-  }
-
-  const handleSecondLastNameChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setSecondLastName(event.target.value)
-    onUpdate(user.dni, 'secondLastName', event.target.value)
-  }
-
   return (
     <div
       ref={setNodeRef}
       style={style}
       {...attributes}
       {...listeners}
-      id={user.dni}
+      id={defaultMember.id}
     >
-      <Input name="name" onChange={handleFirstNameChange} />
-      <TextField
-        label="Cargo"
-        defaultValue={secondLastName}
-        onChange={handleSecondLastNameChange}
-        variant="outlined"
-        style={{ marginRight: 8 }}
-      />
-      <Button onClick={() => onDelete(user.dni)}>Eliminar</Button>
+      <Grid xs={12} md={8}>
+        <Card>
+          <Stack spacing={3} sx={{ p: 3 }}>
+            <Box
+              sx={{
+                columnGap: 2,
+                rowGap: 3,
+                display: 'grid',
+                gridTemplateColumns: {
+                  xs: 'repeat(1, 1fr)',
+                  md: 'repeat(3, 2fr)',
+                },
+              }}
+            >
+              <Controller
+                name={`members.${index}.member`}
+                control={control}
+                render={({ field }) => (
+                  <RHFTextField
+                    label="Miembro"
+                    name={field.name}
+                    value={field.value}
+                    variant="outlined"
+                    style={{ marginRight: 8 }}
+                  />
+                )}
+              />
+
+              <Controller
+                name={`members.${index}.positionName`}
+                control={control}
+                render={({ field }) => (
+                  <RHFTextField
+                    label="Posición"
+                    name={field.name}
+                    value={field.value}
+                    variant="outlined"
+                    style={{ marginRight: 8 }}
+                  />
+                )}
+              />
+
+              <Button onClick={() => onDelete(defaultMember.id)}>
+                Eliminar
+              </Button>
+            </Box>
+          </Stack>
+        </Card>
+      </Grid>
     </div>
   )
 }
