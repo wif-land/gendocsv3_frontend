22d3d78b4170c87a3fa8aaecdfad743295c78415
SrPabvliss <pablomartinvillacres@gmail.com>
2024-04-18 16:33:12 -0500

    Default view modified and needs to be modularized, hook to be cheked

--- src/features/default-members/presentation/view/DefaultMemberSortableItem.tsx
+++ src/features/default-members/presentation/view/DefaultMemberSortableItem.tsx
@@ -1,68 +1,130 @@
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 
-import { Button, Grid, Card, Stack, Box, Typography } from '@mui/material'
-import { RHFTextField } from '../../../../shared/sdk/hook-form'
-import { FieldArrayWithId } from 'react-hook-form'
+import { Button, Card, Stack, Box } from '@mui/material'
+import { IDefaultMembers } from '../../domain/entities/DefaultMembers'
+import { Icon } from '@iconify/react/dist/iconify.js'
 
 export const DefaultMemberSortableItem = ({
   defaultMember,
   onDelete,
-  index,
+  onEdit,
 }: {
-  defaultMember: FieldArrayWithId
+  defaultMember: IDefaultMembers
   onDelete: (id: string) => void
+  onEdit: (id: string) => void
   index: number
 }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: defaultMember.id })
+    useSortable({ id: (defaultMember.member as string).split('-')[1] })
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
   }
 
   return (
-    <div ref={setNodeRef} style={style} id={defaultMember.id}>
-      <Grid xs={12} md={8}>
-        <Card>
-          <Stack spacing={3} sx={{ p: 3 }}>
-            <div {...listeners} {...attributes} style={{ cursor: 'grab' }}>
-              <Typography variant="body2">Arrastre aquí</Typography>
-            </div>
-            <Box
-              sx={{
-                columnGap: 2,
-                rowGap: 3,
-                display: 'grid',
-                gridTemplateColumns: {
-                  xs: 'repeat(1, 1fr)',
-                  md: 'repeat(3, 2fr)',
-                },
-              }}
-            >
-              <RHFTextField
-                id={`members.${index}.member`}
-                label="Miembro"
-                name={`members.${index}.member`}
-                variant="outlined"
-                style={{ marginRight: 8 }}
-              />
-
-              <RHFTextField
-                id={`members.${index}.positionName`}
-                label="Posición"
-                name={`members.${index}.positionName`}
-                variant="outlined"
-                style={{ marginRight: 8 }}
-              />
-
-              <Button onClick={() => onDelete(defaultMember.id)}>
-                Eliminar
-              </Button>
+    <div
+      ref={setNodeRef}
+      style={style}
+      id={(defaultMember.member as string).split('-')[1]}
+    >
+      <Card variant="outlined">
+        <Stack
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: '8% repeat(2, 1fr) 15%',
+            gap: 2,
+            alignItems: 'center',
+            p: 2,
+          }}
+        >
+          <Box
+            {...listeners}
+            {...attributes}
+            style={{ cursor: 'grab' }}
+            ref={setNodeRef}
+            id={(defaultMember.member as string).split('-')[1]}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <Icon
+              icon="mdi:drag-vertical-variant"
+              width="1.2rem"
+              height="1.2rem"
+            />
+            <Icon icon="lets-icons:user-box-duotone" width="48" height="48" />
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <Box component="div" color="inherit">
+              {`${(defaultMember.member as string).split('-')[0]} `}
             </Box>
-          </Stack>
-        </Card>
-      </Grid>
+            <Box
+              component="div"
+              sx={{ typography: 'subtitle2', color: 'text.disabled' }}
+            >
+              {`  - ${(defaultMember.member as string).split('-')[1]}`}
+            </Box>
+          </Box>
+
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              ml: 2,
+              gap: 1,
+            }}
+          >
+            <Box component="div" color="inherit">
+              Posición asignada:
+            </Box>
+            <Box
+              component="div"
+              sx={{ typography: 'subtitle2', color: 'text.disabled' }}
+            >
+              {` ${defaultMember.positionName}`}
+            </Box>
+          </Box>
+
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+              gap: 2,
+              mr: 2,
+            }}
+          >
+            <Button
+              variant="outlined"
+              color="info"
+              onClick={() =>
+                onEdit((defaultMember.member as string).split('-')[1])
+              }
+            >
+              Editar
+            </Button>
+            <Button
+              onClick={() =>
+                onDelete((defaultMember.member as string).split('-')[1])
+              }
+              variant="outlined"
+              color="error"
+              sx={{ px: 5 }}
+            >
+              Eliminar
+            </Button>
+          </Box>
+        </Stack>
+      </Card>
     </div>
   )
 }
--- src/features/default-members/presentation/view/DefaultMembersView.tsx
+++ src/features/default-members/presentation/view/DefaultMembersView.tsx
@@ -1,59 +1,159 @@
-import { Box, Button, CardHeader, Container, Stack } from '@mui/material'
+import { Alert, Box, Button, Card, Container, Stack } from '@mui/material'
 import { DndContext, closestCenter } from '@dnd-kit/core'
 import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 import { DefaultMemberSortableItem } from './DefaultMemberSortableItem'
 import { useDefaultMembersV2 } from '../hooks/useDefaultV2'
 import FormProvider from '../../../../shared/sdk/hook-form/form-provider'
+import { RHFTextField } from '../../../../shared/sdk/hook-form'
+import CustomBreadcrumbs from '../../../../shared/sdk/custom-breadcrumbs/custom-breadcrumbs'
+import { paths } from '../../../../core/routes/paths'
 
 const DefaultMembersView: React.FC = () => {
   const {
     handleSubmit,
-    fields,
-    handleAddMember,
     handleRemoveMember,
     methods,
     onSubmit,
     handleDragEnd,
+    formattedItems,
+    sendData,
+    areThereChanges,
+    handleDiscardChanges,
+    handleEditMember,
+    isEditMode,
   } = useDefaultMembersV2()
 
   return (
     <Container>
-      <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-        <CardHeader title="Detalles" />
-        <SortableContext
-          items={fields.map((item) => item.id)}
-          strategy={verticalListSortingStrategy}
+      <CustomBreadcrumbs
+        heading="Miembros por defecto"
+        links={[
+          {
+            name: 'Dashboard',
+            href: paths.dashboard.root,
+          },
+          { name: 'Miembros por defecto' },
+        ]}
+        sx={{
+          mb: { xs: 3, md: 5 },
+        }}
+      />
+      <Stack spacing={3} sx={{ p: 3 }}>
+        <Card
+          sx={{
+            p: 3,
+          }}
         >
+          <Alert
+            severity="info"
+            variant="outlined"
+            sx={{
+              mb: 3,
+            }}
+          >
+            Añade los miembros por defecto que pertenecerán al módulo. El orden
+            de los miembros se puede modificar arrastrando y soltando
+          </Alert>
           <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-            <Stack spacing={3} sx={{ p: 3 }}>
-              <Box
-                sx={{
-                  columnGap: 2,
-                  rowGap: 3,
-                  display: 'flex',
-                  flexDirection: 'column',
-                }}
-              >
-                {fields.map((item, index) => (
-                  <DefaultMemberSortableItem
-                    defaultMember={item}
-                    onDelete={handleRemoveMember}
-                    key={index}
-                    index={index}
-                  />
-                ))}
+            <Box
+              sx={{
+                columnGap: 2,
+                rowGap: 3,
+                display: 'flex',
+                flexDirection: 'row',
+              }}
+            >
+              <RHFTextField
+                id={`member`}
+                label="Miembro"
+                name={`member`}
+                variant="outlined"
+              />
 
-                <Button onClick={handleAddMember} style={{ marginBottom: 16 }}>
+              <RHFTextField
+                id={`positionName`}
+                label="Posición"
+                name={`positionName`}
+                variant="outlined"
+              />
+              {isEditMode.value ? (
+                <Button
+                  onClick={methods.handleSubmit(onSubmit)}
+                  variant="contained"
+                  sx={{ width: '50%' }}
+                >
+                  Editar
+                </Button>
+              ) : (
+                <Button type="submit" variant="contained" sx={{ width: '50%' }}>
                   Agregar Nuevo
                 </Button>
-                <Button type="submit" variant="contained">
-                  Guardar
-                </Button>
-              </Box>
-            </Stack>
+              )}
+            </Box>
           </FormProvider>
-        </SortableContext>
-      </DndContext>
+          <DndContext
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+          >
+            <SortableContext
+              items={formattedItems.map((item) => item.member.split('-')[1])}
+              strategy={verticalListSortingStrategy}
+            >
+              <Stack spacing={3} sx={{ py: 3 }}>
+                <Box
+                  sx={{
+                    columnGap: 2,
+                    rowGap: 3,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    px: 3,
+                  }}
+                >
+                  {formattedItems.map((item, index) => (
+                    <DefaultMemberSortableItem
+                      defaultMember={item}
+                      onDelete={handleRemoveMember}
+                      onEdit={handleEditMember}
+                      key={index}
+                      index={index}
+                    />
+                  ))}
+                  {areThereChanges.value && (
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        width: '100%',
+                        gap: 2,
+                      }}
+                    >
+                      <Button
+                        onClick={handleDiscardChanges}
+                        variant="outlined"
+                        color="error"
+                        sx={{
+                          flexGrow: 1,
+                        }}
+                      >
+                        Descartar
+                      </Button>
+                      <Button
+                        onClick={sendData}
+                        variant="contained"
+                        sx={{
+                          flexGrow: 1,
+                        }}
+                      >
+                        Guardar
+                      </Button>
+                    </Box>
+                  )}
+                </Box>
+              </Stack>
+            </SortableContext>
+          </DndContext>
+        </Card>
+      </Stack>
     </Container>
   )
 }
