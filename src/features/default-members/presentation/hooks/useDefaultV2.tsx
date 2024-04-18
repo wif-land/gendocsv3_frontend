33a5ea804628bac45ebcacff25f16a02d1a7fd21
SrPabvliss <pablomartinvillacres@gmail.com>
2024-04-18 16:33:12 -0500

    Default view modified and needs to be modularized, hook to be cheked

--- src/features/default-members/presentation/hooks/useDefaultV2.tsx
+++ src/features/default-members/presentation/hooks/useDefaultV2.tsx
@@ -1,11 +1,15 @@
-import { useForm, useFieldArray } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import * as Yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { IDefaultMembers, IMember } from '../../domain/entities/DefaultMembers'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useBoolean } from '../../../../shared/hooks/use-boolean'
 
 export const useDefaultMembersV2 = () => {
-  const [items] = useState<IDefaultMembers[]>([
+  const areThereChanges = useBoolean()
+  const isEditMode = useBoolean()
+
+  const [fetchedItems] = useState<IDefaultMembers[]>([
     {
       positionName: 'Developer',
       order: 1,
@@ -32,87 +36,138 @@ export const useDefaultMembersV2 = () => {
     },
   ])
 
+  const [formattedItems, setFormattedItems] = useState(
+    fetchedItems.map((item) => ({
+      ...item,
+      member: `${(item.member as IMember).firstName} ${
+        (item.member as IMember).firstLastName
+      } ${(item.member as IMember).secondLastName} - ${
+        (item.member as IMember).dni
+      }`,
+    })),
+  )
+
+  const [addedMembers, setAddedMembers] = useState<IDefaultMembers[]>([])
+  const [removedMembers, setRemovedMembers] = useState<IDefaultMembers[]>([])
+
   const schema = Yup.object().shape({
-    members: Yup.array().of(
-      Yup.object().shape({
-        order: Yup.number().required(),
-        positionName: Yup.string().required(),
-        member: Yup.mixed().required(),
-      }),
-    ),
+    member: Yup.string().required('Este campo es requerido'),
+    positionName: Yup.string().required('Este campo es requerido'),
   })
 
   const methods = useForm({
     resolver: yupResolver(schema),
     defaultValues: {
-      members: items.map((item) => ({
-        order: item.order,
-        positionName: item.positionName,
-        member: `${(item.member as IMember).firstName} ${
-          (item.member as IMember).secondName
-        } ${(item.member as IMember).firstLastName || ''} ${
-          (item.member as IMember).secondLastName || ''
-        } - ${(item.member as IMember).dni || ''} `,
-      })),
+      member: '',
+      positionName: '',
     },
   })
 
-  const { fields, remove } = useFieldArray({
-    control: methods.control,
-    name: 'members',
-  })
-
   const handleAddMember = () => {
-    methods.setValue(
-      'members',
-      methods.getValues().members?.concat({
-        order: fields.length + 1,
-        positionName: '',
-        member: '',
-      }),
+    const newMember = {
+      positionName: methods.getValues('positionName'),
+      member: methods.getValues('member'),
+      order: formattedItems.length + 1,
+    }
+
+    setAddedMembers((prev) => [...prev, newMember])
+    setFormattedItems((prev) => [...prev, newMember])
+  }
+
+  const handleEditMember = (id: string) => {
+    const editedMember = formattedItems.find(
+      (item) => item.member.split('-')[1] === id,
     )
+    methods.setValue('member', editedMember?.member as string)
+    methods.setValue('positionName', editedMember?.positionName as string)
+    handleRemoveMember(id)
+    isEditMode.onTrue()
   }
 
-  const handleRemoveMember = (index: string) => {
-    // buscar el index del elemento a eliminar
-    console.log(index)
-    const indexToRemove = fields.findIndex((field) => field.id === index)
-    console.log(indexToRemove)
-    remove(indexToRemove)
+  const handleRemoveMember = (id: string) => {
+    const removedMember = formattedItems.find(
+      (item) => item.member.split('-')[1] === id,
+    )
+    setFormattedItems((prev) =>
+      prev.filter((item) => item.member.split('-')[1] !== id),
+    )
+    setRemovedMembers((prev) => [...prev, removedMember as IDefaultMembers])
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const onSubmit = (data: any) => {
-    console.log(data)
+  const onSubmit = () => {
+    isEditMode.value === true && isEditMode.onFalse()
+    handleAddMember()
+    methods.reset()
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleDragEnd = (event: any) => {
     const { active, over } = event
     if (active.id !== over.id) {
-      const oldIndex = fields.findIndex((item) => item.id === active.id)
-      const newIndex = fields.findIndex((item) => item.id === over.id)
-      const oldOrder = fields[oldIndex].order
-      const newOrder = fields[newIndex].order
+      const activeIndex = formattedItems.findIndex(
+        (item) => item.member.split('-')[1] === active.id,
+      )
+      const overIndex = formattedItems.findIndex(
+        (item) => item.member.split('-')[1] === over.id,
+      )
+      const oldOrder = formattedItems[activeIndex].order
+      const newOrder = formattedItems[overIndex].order
 
-      fields[oldIndex].order = newOrder
-      fields[newIndex].order = oldOrder
-
-      const newFields = [...fields]
-      newFields[oldIndex] = fields[newIndex]
-      newFields[newIndex] = fields[oldIndex]
-      console.log({ newFields }, { oldIndex })
-      methods.setValue('members', newFields)
+      const newItems = [...formattedItems]
+      const [removed] = newItems.splice(activeIndex, 1)
+      newItems.splice(overIndex, 0, removed)
+      newItems[overIndex].order = newOrder
+      newItems[activeIndex].order = oldOrder
+      setFormattedItems(newItems)
     }
   }
+
+  const handleDiscardChanges = () => {
+    setFormattedItems(
+      fetchedItems.map((item) => ({
+        ...item,
+        member: `${(item.member as IMember).firstName} ${
+          (item.member as IMember).firstLastName
+        } ${(item.member as IMember).secondLastName} - ${
+          (item.member as IMember).dni
+        }`,
+      })),
+    )
+    setAddedMembers([])
+    setRemovedMembers([])
+    isEditMode.onFalse()
+    areThereChanges.onFalse()
+    methods.reset()
+  }
+
+  const sendData = () => {
+    console.log(removedMembers)
+    console.log(addedMembers)
+    console.log(formattedItems)
+  }
+
+  useEffect(() => {
+    if (
+      addedMembers.length > 0 ||
+      removedMembers.length > 0 ||
+      isEditMode.value === true
+    ) {
+      areThereChanges.onTrue()
+    }
+  }, [addedMembers, removedMembers, isEditMode.value])
 
   return {
     ...methods,
     methods,
-    fields,
     handleAddMember,
     handleRemoveMember,
     onSubmit,
     handleDragEnd,
+    formattedItems,
+    sendData,
+    areThereChanges,
+    handleEditMember,
+    handleDiscardChanges,
+    isEditMode,
   }
 }
