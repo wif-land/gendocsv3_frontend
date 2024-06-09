629df45a5f8d0e088e493037080a88ff39399918
leninner <mazabandalenin180@gmail.com>
2024-06-08 23:52:05 -0500

    Add welcome labels

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,45 +7,7 @@ interface LayoutProps {
   children: React.ReactNode
 }
 const layout: React.FC<LayoutProps> = ({ children }) => (
-  <div>
-    <DashboardLayout>{children}</DashboardLayout>
-    {/*
-      <Suspense fallback={<LoadingScreen />}>
-        <PrivateRoute>
-          <div className="h-screen flex">
-            <nav className="fixed top-0 z-50 py-5 w-full bg-red-800 flex justify-end items-center px-10">
-              <Avatar
-                icon={<AvatarIcon />}
-                classNames={{
-                  base: 'mx-10',
-                  icon: 'text-black/80',
-                }}
-              />
-
-              <div
-                className="text-white font-sm cursor-pointer hover:text-black/80 duration-100 transition-colors px-2 text-center"
-                onClick={logout}
-              >
-                Cerrar sesión
-              </div>
-            </nav>
-
-            <div className="flex mt-20 h-full w-full">
-              <aside className="max-w-fit bg-white h-full overflow-x-auto px-3">
-                <ScrollShadow hideScrollBar className="h-full overflow-auto">
-                  <ul>
-                    <CareerModule />
-                  </ul>
-                </ScrollShadow>
-              </aside>
-
-              <Divider orientation="vertical" />
-              <section className="flex-grow p-4">{children}</section>
-            </div>
-          </div>
-        </PrivateRoute>
-      </Suspense> */}
-  </div>
+  <DashboardLayout>{children}</DashboardLayout>
 )
 
 export default layout
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,34 @@
-const DashboardPage = () => <div>Dashboard</div>
+'use client'
 
-export default DashboardPage
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Unstable_Grid2'
+
+import SeoIllustration from '../../shared/assets/illustrations/seo-illustration'
+import AppWelcome from './app-welcome'
+import { useSettingsContext } from '../../shared/sdk/settings'
+import { useAccountStore } from '../../features/auth/presentation/state/useAccountStore'
+
+export default () => {
+  const settings = useSettingsContext()
+  const { user } = useAccountStore()
+
+  return (
+    <Container maxWidth={settings.themeStretch ? false : 'xl'}>
+      <Grid container spacing={3}>
+        <Grid xs={12} md={8}>
+          <AppWelcome
+            title={`Hola 👋, ${user?.firstName} ${user?.firstLastName}`}
+            description="Bienvenido a tu panel de control, aquí podrás gestionar los documentos"
+            img={<SeoIllustration />}
+            action={
+              <Button variant="contained" color="primary">
+                Empezar
+              </Button>
+            }
+          />
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
