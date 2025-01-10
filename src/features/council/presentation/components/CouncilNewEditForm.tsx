edbe64946351a60167a8bd66208c08e2c4e3fd8a
devjaes <merajair17@gmail.com>
2025-01-10 03:58:03 -0500

    remove past block on date council creation

--- src/features/council/presentation/components/CouncilNewEditForm.tsx
+++ src/features/council/presentation/components/CouncilNewEditForm.tsx
@@ -97,7 +97,6 @@ export const CouncilNewEditForm = ({ currentCouncil }: Props) => {
                       required: true,
                     },
                   }}
-                  disablePast
                 />
               )}
             />
