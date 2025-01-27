d050566cfb75f35521b69f9d96b720101d9c37e4
devjaes <merajair17@gmail.com>
2025-01-27 17:04:19 -0500

    updata docsByStudent table

--- src/features/documents/presentation/components/DocsByStudentTableRow.tsx
+++ src/features/documents/presentation/components/DocsByStudentTableRow.tsx
@@ -7,6 +7,8 @@ import TableCell from '@mui/material/TableCell'
 import ListItemText from '@mui/material/ListItemText'
 
 import { DocumentModel } from '../../data/models/DocumentsModel'
+import { format } from 'date-fns'
+import { Stack } from '@mui/material'
 
 type Props = {
   row: DocumentModel
@@ -30,8 +32,51 @@ export const DocsByStudentTableRow = ({ row, selected }: Props) => (
 
       <TableCell>
         <ListItemText
-          primary={row.description}
+          primary={format(
+            new Date(row.createdAt || Date.now()),
+            'dd/MM/yyyy hh:mm',
+          )}
           primaryTypographyProps={{ typography: 'body2', noWrap: true }}
+        />
+      </TableCell>
+
+      <TableCell sx={{ alignItems: 'center' }}>
+        <ListItemText
+          disableTypography
+          primaryTypographyProps={{ typography: 'body2', noWrap: false }}
+          primary={row.userName ?? 'N/A'}
+        />
+      </TableCell>
+
+      <TableCell sx={{ alignItems: 'center' }}>
+        <ListItemText
+          disableTypography
+          primary={
+            <Stack
+              direction="row"
+              spacing={1}
+              alignItems="center"
+              justifyContent="start"
+            >
+              {row.councilName || 'N/A'}
+            </Stack>
+          }
+        />
+      </TableCell>
+
+      <TableCell sx={{ alignItems: 'center' }}>
+        <ListItemText
+          disableTypography
+          primary={
+            <Stack
+              direction="row"
+              spacing={1}
+              alignItems="center"
+              justifyContent="start"
+            >
+              {row.templateName || 'N/A'}
+            </Stack>
+          }
         />
       </TableCell>
     </TableRow>
